b866ddff5f0a42b8b0e30bfe544c985af74eca22
infinitechlabs <phpenquiry@gmail.com>
2026-05-27 12:08:36 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical chatbot",
+    version="1.0.0",
+    author="user",
+    author_email="phpenquiry@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
